90b596bbc2c935fb550a6df72938be7c4bcd0c4a
Alp <47161013+Aegispaces@users.noreply.github.com>
2023-11-26 19:10:49 +0300

    copyclipboard.sh

---
+++ copyclipboard.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Open hyprpicker to pause the screen
+hyprpicker -r -z &
+
+# Store the process ID of hyprpicker
+hyprpicker_pid=$!
+
+# Wait for hyprpicker to start
+sleep 0.1
+
+# Open hyprshot to select a region
+hyprshot -m region --clipboard-only
+
+# Store the process ID of hyprshot
+hyprshot_pid=$!
+
+# Wait for the hyprshot process to finish
+wait $hyprshot_pid
+
+# Wait for hyprpicker to start
+sleep 0.1
+
+# Close hyprpicker
+kill $hyprpicker_pid
+
+# Close hyprshot
+kill $hyprshot_pid
